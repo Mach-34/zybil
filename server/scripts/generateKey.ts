b9eab64edfb675adeb3dbbb93121ccdf4a5d43c0
Jack Gilcrest <me@jp4g.llc>
2023-10-17 08:09:16 -0600

    bridge in getting there

---
+++ server/scripts/generateKey.ts
@@ -0,0 +1,17 @@
+import { GrumpkinScalar } from "@aztec/aztec.js";
+
+// why is this async
+async function main(): Promise<string> {
+    const seed = GrumpkinScalar.random();
+    return seed.toString().slice(2);
+}
+
+main()
+    .then((key: string) => {
+        console.log(key);
+        process.exit(0);
+    })
+    .catch((err: Error) => {
+        console.error(err);
+        process.exit(1);
+    });
