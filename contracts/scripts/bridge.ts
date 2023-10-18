5e202576c818384cf41a86eec1b66f91137538ac
Jack Gilcrest <me@jp4g.llc>
2023-10-18 12:34:30 -0600

    push

---
+++ contracts/scripts/bridge.ts
@@ -0,0 +1,28 @@
+import { ethers, run } from 'hardhat';
+import { Signer } from 'ethers';
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+async function main() {
+    const network = await ethers.provider.getNetwork();
+    console.log(`====================================`);
+    console.log(`Deploying Zybil L1 Contracts to ${network.name} (${network.chainId})...`)
+    // get deployer wallet
+    const [deployer]: Signer[] = await ethers.getSigners();
+
+    // deploy groth16 verifier
+    const ensFactory = await ethers.getContractFactory('ToyENS');
+    const ens = await ensFactory.deploy();
+    await ens.waitForDeployment();
+    const ensAddress = await ens.getAddress();
+
+    // deploy zybil
+    console.log(`Deployed ENS Contract to ${ensAddress}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
