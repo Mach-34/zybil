493f3568a4a1490d6743a77d6af4a76b6f44e83f
Ian-Bright <ianbrighton1@gmail.com>
2023-10-21 15:10:03 -0600

    get rest of server

--- server/scripts/verifySignatureOnChain.ts
+++ server/scripts/verifySignatureOnChain.ts
@@ -53,40 +53,38 @@ const encodeVerifiedData = (stampType: number, id: string) => {
 
 const msgToBytes = (msg: Array<any>) => {
     const bytes = new Uint8Array(33);
-    bytes.set(msg[0], 0)
-    bytes.set(Uint8Array.from(msg[1]), 1);
+    bytes.set([msg[0]], 0)
+    bytes.set(Uint8Array.from(Buffer.from((`0x${msg[1]}` as string), 'hex')), 1);
     return bytes
 }
 
 const generateSignatureAndMsg = async (privkey: GrumpkinScalar) => {
     const msg = encodeVerifiedData(2, 'Test@gmail.com');
-    console.log('Msg: ', msg);
-    // const bytes = msgToBytes(msg);
-    // console.log('Bytes: ', bytes);
-    // const schnorr = await Schnorr.new();
-    // const signature = schnorr.constructSignature(bytes, privkey);
-    // const signatureBytes = new Uint8Array(signature.toBuffer());
-    // // @ts-ignore
-    // msg[0] = msg[0] as FieldLike;
-    // // @ts-ignore
-    // msg[1] = Buffer.from(`${msg[1]}`, 'hex') as FieldLike;
-    // return { msg: msg, signature: Array.from(signatureBytes) }
+    const bytes = msgToBytes(msg);
+    const schnorr = await Schnorr.new();
+    const signature = schnorr.constructSignature(bytes, privkey);
+    const signatureBytes = new Uint8Array(signature.toBuffer());
+    // @ts-ignore
+    msg[0] = msg[0] as FieldLike;
+    // @ts-ignore
+    msg[1] = Buffer.from(`0x${msg[1]}`, 'hex') as FieldLike;
+    return { msg: msg, signature: Array.from(signatureBytes) }
 }
 
 
 async function main() {
-    // const pubkey = await generatePublicKey(GrumpkinScalar.fromString(privkey))
-    // const client = createPXEClient(sandboxURL);
-    // const zybil = await deployContract(client as Wallet, pubkey);
+    const pubkey = await generatePublicKey(GrumpkinScalar.fromString(privkey))
+    const client = createPXEClient(sandboxURL);
+    const zybil = await deployContract(client as Wallet, pubkey);
 
 
-    await generateSignatureAndMsg(GrumpkinScalar.fromString(privkey));
+    const { msg, signature } = await generateSignatureAndMsg(GrumpkinScalar.fromString(privkey));
 
-    // const verified = await zybil.methods.valid_signature(
-    //     signature,
-    //     msg
-    // ).view();
-    // console.log('Decoded: ', verified);dd4964108fff624045ebaa1358825cb01ce95d6
+    const verified = await zybil.methods.valid_signature(
+        signature,
+        msg
+    ).view();
+    console.log('Decoded: ', verified);
 }
 
 main()
